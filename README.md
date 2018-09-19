79dc58b405af7ecbd84f570ade2e09317c9e5632
kickscondor <kicks@kickscondor.com>
2018-09-19 12:01:14 -0600

    My initial changes to allow `readdir` and `stat` to be plugged.

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
 # fswalk
 
-> The simplest possible directory walker
-
-[![Build Status](https://img.shields.io/travis/mcfedr/fswalk.svg?style=flat-square)](https://travis-ci.org/mcfedr/fswalk)
-[![npm version](https://img.shields.io/npm/v/fswalk.svg?style=flat-square)](https://npmjs.org/package/fswalk)
-
+> A simple, flexible directory walker that can be decoupled from the 'fs'
+> module.
 
 ## Installation
 
 ```bash
-npm install fswalk
+npm install plugwalk
 ```
 
 ## Example
 
 ```js
-var walk = require('fswalk');
+var walk = require('plugwalk');
 
 walk(__dirname, function(path, stats) {
   console.log(path, stats);
@@ -26,17 +23,16 @@ walk(__dirname, function(path, stats) {
 
 ## API
 
-#### `walk(dir, walkCb, finishCb, limit)`
+#### `walk(dir, walkCb, finishCb, limit, readdir, stat)`
 
 ##### Parameters
 
 * `dir`: The directory to walk
 * `walkCb`: `function(file, stats[, callback])` The function to be called with each file.
-
-* * `file`:  Relative file path
-* * `stats`: [fs.Stats](https://nodejs.org/docs/latest/api/fs.html#fs_class_fs_stats) object
-
-* * `callback`: If given, any errors will be propagated to `finishCb` and the walk will stop soon. `walkCb` is called in parallel for some files, so the walk will stop at the current folder. `finishCb` will not be called until all `walkCb` callbacks have been called.
-
+  * `file`:  Relative file path.
+  * `stats`: [fs.Stats](https://nodejs.org/docs/latest/api/fs.html#fs_class_fs_stats) object
+  * `callback`: If given, any errors will be propagated to `finishCb` and the walk will stop soon. `walkCb` is called in parallel for some files, so the walk will stop at the current folder. `finishCb` will not be called until all `walkCb` callbacks have been called.
 * `finishCb`: `function(err)` Called when all files have been walked.
-* `limit`: Max depth, `undefined` means no limit
+* `limit`: Max depth, `undefined` means no limit.
+* `readdir`: Replacement for `fs.readdir` call.
+* `stat`: Replacement for `fs.stat` call.
--- walk.js
+++ walk.js
@@ -1,66 +1,69 @@
 var fs = require('fs'),
-    path = require('path');
+  path = require('path');
 
-module.exports = function walk(dir, walkCb, finishCb, limit) {
-    function callWalkCb(file, stats, cb) {
-        if (walkCb.length == 3) {
-            walkCb(file, stats, cb);
-        } else {
-            walkCb(file, stats);
-            cb();
-        }
+module.exports = function walk(dir, walkCb, finishCb, limit, readdir, stat, dirlen) {
+  dirlen = dirlen || dir.length + 1;
+  readdir = readdir || fs.readdir;
+  stat = stat || fs.stat;
+  function callWalkCb(file, stats, cb) {
+    if (walkCb.length == 3) {
+      walkCb(file, stats, cb);
+    } else {
+      walkCb(file, stats);
+      cb();
     }
+  }
 
-    function callFinishCb(err) {
-        finishCb && finishCb(err);
+  function callFinishCb(err) {
+    finishCb && finishCb(err);
+  }
+
+  stat(dir, function(err, stats) {
+    if(err) {
+      callFinishCb(err);
+      return;
     }
-
-    fs.stat(dir, function(err, stats) {
+    if(stats.isDirectory()) {
+      if (limit !== undefined && limit == 0) {
+        callFinishCb();
+        return;
+      }
+      readdir(dir, function(err, files) {
+        var count = files.length + 1,
+          ok = true;
         if(err) {
-            callFinishCb(err);
-            return;
+          callFinishCb(err);
+          return;
         }
-        if(stats.isDirectory()) {
-            if (limit !== undefined && limit == 0) {
-                callFinishCb();
-                return;
+
+        files.forEach(function(file) {
+          walk(path.join(dir, file), walkCb, function(err) {
+            if(err) {
+              fail(err);
+              return;
             }
-            fs.readdir(dir, function(err, files) {
-                var count = files.length + 1,
-                    ok = true;
-                if(err) {
-                    callFinishCb(err);
-                    return;
-                }
+            after();
+          }, limit > 0 ? limit - 1 : limit, readdir, stat, dirlen);
+        });
 
-                files.forEach(function(file) {
-                    walk(path.join(dir, file), walkCb, function(err) {
-                        if(err) {
-                            fail(err);
-                            return;
-                        }
-                        after();
-                    }, limit > 0 ? limit - 1 : limit);
-                });
+        after();
 
-                after();
-
-                function fail(err) {
-                    if(ok) {
-                        ok = false;
-                        callFinishCb(err);
-                    }
-                }
-
-                function after() {
-                    if(ok && --count == 0) {
-                        callFinishCb();
-                    }
-                }
-            });
+        function fail(err) {
+          if(ok) {
+            ok = false;
+            callFinishCb(err);
+          }
         }
-        else {
-            callWalkCb(dir, stats, callFinishCb);
+
+        function after() {
+          if(ok && --count == 0) {
+            callFinishCb();
+          }
         }
-    });
+      });
+    }
+    else {
+      callWalkCb(dir.slice(dirlen), stats, callFinishCb);
+    }
+  });
 };
